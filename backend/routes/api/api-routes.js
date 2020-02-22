139a527d804ddc7d9f808e38312f3d55f7e8976e
William White <william.white86@gmail.com>
2020-02-21 23:46:24 -0500

    added PUT and POST api routes

--- backend/routes/api/api-routes.js
+++ backend/routes/api/api-routes.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 // **look in path**
 const path = require('path');
 const db = require('../../models');
+
 // Performances data-entry
 // GET index, all performances
 router.get('/performances', (req, res) => {
@@ -12,6 +13,43 @@ router.get('/performances', (req, res) => {
             console.log(performances);
             res.json(performances);
         });
+});
+
+//POST create, create a new performance
+router.post('/performances', (req, res) => {
+    const performance = req.body;
+    db.Performances.create(performance)
+        .then((results) => {
+            res.json({
+                success: true,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success: false,
+                errors: err.errors,
+            });
+        });
+});
+
+//PUT update, update an existing performance by id
+router.put('/performances/:id', (req, res) => {
+    db.Performances.update(req.body,
+    {
+        where: {
+            id: req.body.id
+            },
+    }).then((response) => {
+        res.json({
+            success:true,
+        });
+    })
+    .catch((err) =>{
+        res.status(500).json({
+            success: false,
+            errors: err.errors,
+        })
+    })
 });
 
 module.exports = router;
