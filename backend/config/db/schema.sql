39d299e43bbd5bad3972662442972e11e11ef988
William White <william.white86@gmail.com>
2020-02-27 11:57:31 -0500

    updated schema

--- backend/config/db/schema.sql
+++ backend/config/db/schema.sql
@@ -31,12 +31,26 @@ CREATE TABLE performances (
 );
 
 CREATE TABLE musicAPIs (
-    id Int( 11 ) AUTO_INCREMENT NOT NULL,
+    id Int AUTO_INCREMENT NOT NULL,
     performanceID INT NOT NULL,
     spotify INTEGER NOT NULL,
     youtube INTEGER NOT NULL,
     instagram INTEGER NOT NULL,
+	createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updatedAt TIMESTAMP NOT NULL DEFAULT current_timestamp,
     /* Set ID as primary key */
     PRIMARY KEY ( id ),
     FOREIGN KEY (performanceID) references performances(id)
 );
+
+INSERT INTO performances (performance, date, total_sold, spotify_reach, instagram_following, total_money, total_attendance, average_ticket_price, percent_sold, show_success, fiscal_year, genre, spotify_popularity, instagram_strength, average_strength, projected_success, actual_success, predictability) 
+VALUES ("Pat Benatar & Neil Giraldo", "3/8/17", 689, "5-7K", 23200, 59537, 715, 86,	83,	True, "FY17", "Rock", 5, 4, 4.5, "Great", "Great", "Correct");
+
+INSERT INTO performances (performance, date, total_sold, spotify_reach, instagram_following, total_money, total_attendance, average_ticket_price, percent_sold, show_success, fiscal_year, genre, spotify_popularity, instagram_strength, average_strength, projected_success, actual_success, predictability) 
+VALUES ("The Tallest Man on Earth", "5/10/20", 244, "<2K", 241,	9941, 244, 41, 29,NULL,"FY20", "Singer/Songwriter", 1, 1, 1, "Poor", "Poor", "Correct");
+
+INSERT INTO performances (performance, date, total_sold, spotify_reach, instagram_following, total_money, total_attendance, average_ticket_price, percent_sold, show_success, fiscal_year, genre, spotify_popularity, instagram_strength, average_strength, projected_success, actual_success, predictability) 
+VALUES ("Josh Turner", "4/5/20", 102, "6-8K", 125000, 14565, 194, 143, 12, NULL, "FY20", "Country", 5, 5, 5, "Great", "Poor", "Wrong");
+
+INSERT INTO performances (performance, date, total_sold, spotify_reach, instagram_following, total_money, total_attendance, average_ticket_price, percent_sold, show_success, fiscal_year, genre, spotify_popularity, instagram_strength, average_strength, projected_success, actual_success, predictability) 
+VALUES ("Bruce Hornsby and Music", "3/1/20", 351, "<2K", 10000, 29670, 365, 85, 42, NULL, "FY20", "Rythem & Blues", 1, 3, 2, "Good", "Good", "Correct");
