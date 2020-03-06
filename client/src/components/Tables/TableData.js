e4f1516972aed19ee3082e07e7c4bc3b4ff516de
Shakur <miiro.shakur@gmail.com>
2020-03-05 21:19:28 -0500

    grid from API

--- client/src/components/Tables/TableData.js
+++ client/src/components/Tables/TableData.js
@@ -114,59 +114,7 @@ export function TableData({ children }) {
 
 
 
-// import React from "react";
-// import {
-//   GridComponent, 
-//   ColumnDirective, 
-//   ColumnsDirective,
-//   Page,
-//   PageSettingsModel,
-//   Inject,
-//   Filter,
-//   Group
-// } from '@syncfusion/ej2-react-grids';
-// import data from './dataSource.json';
-// import './Table.css';
 
-// // css code url("https://cdn.syncfusion.com/ej2/material.css")
-
-// // the Json object keys are rendered as the header, and the values as rows. 
-// export default class MyGrid extends React.Component {
-
-//   pageOptions:PageSettingsModel={
-
-//     pageSize:8, pageSizes:true
-//   };
-  
-  
-//   render(){
-   
-//     return<GridComponent dataSource={data}
-//         allowPaging={true}
-//         height={268}
-//         pageSettings={this.pageOptions}
-//         pageSize={true}
-//         allowFiltering={true}
-//         allowGroupin={true}
-    
-//     >
-//       <ColumnsDirective>
-//          <ColumnDirective field='ID' headerText='ID' textAlign='Right' width='100' />
-//          <ColumnDirective field='Customer' headerText='Customer' width='150' />
-//          <ColumnDirective field='Performance' headerText='Performance' />
-//          <ColumnDirective field='Date' headerText='Date' textAlign='Right' format='c2' width='100' />
-        
-        
-//       </ColumnsDirective >
-//       <Inject services ={[Page,Filter, Group]}/>
-
-    
-//     </GridComponent>
-   
-//   }
-
-
-// }
 
 
 
