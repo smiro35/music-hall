1535f8d38971b51b6398a0dd0ed38db7ae43ab4a
Kolon Diallo <Kolon@MacBook-Pro.local>
2020-03-06 19:13:24 -0500

    login front end

--- client/src/AuthContext.js
+++ client/src/AuthContext.js
@@ -20,6 +20,7 @@ export const AuthProvider = ({ children }) => {
                     setIsAuth(false)
                 }
             })
+            .catch(err => console.log("Error"))
     }
 
     const logout = async () => {
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,63 +1,193 @@
-import React from 'react';
-import { form, span } from 'react-bootstrap/Form';
-import './css/main.css'
-import './css/util.css'
-import pic from './images/img-01.png';
-import './vendor/animate/animate.css';
-import MyNavbar from "../../components/Navbar/Navbar.js";
+import React, { useContext, useState } from "react";
+import { AuthContext } from '../../AuthContext';
+// import "../App.css";
+import { Container, Row, Button, Col } from "react-bootstrap";
+import Axios from "axios";
 
-function home() {
+function Home(props) {
+
+  const { isAuth, logout } = useContext(AuthContext);
+
+  const [secret, setSecret] = useState("");
+
+  // this function is duplicated in the Members page component
+  // consider refactor 
+  const getSecret = async () => {
+    const secretResponse = await Axios.get("/api/secrets");
+    console.log(secretResponse.data);
+    setSecret(secretResponse.data);
+  };
+
   return (
-    <>
-    <MyNavbar/>
-      <div className="limiter">
-        <div className="container-login100">
-          <div className="wrap-login100">
-              <img src={pic} alt="not working" />
-              <form className="login100-form validate-form">
-                <span className="login100-form-title"> Member Login </span>
-                <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                  <input className="input100" type="text" name="email" placeholder="Email" />
-                  <span className="focus-input100"></span>
-                  <span className="symbol-input100">
-                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                  </span>
-                </div>
-                <div className="wrap-input100 validate-input" data-validate="Password is required">
-                  <input className="input100" type="password" name="pass" placeholder="Password" />
-                  <span className="focus-input100"></span>
-                  <span className="symbol-input100">
-                    <i className="fa fa-lock" aria-hidden="true"></i>
-                  </span>
-                </div>
-                <div className="container-login100-form-btn">
-                  <button className="login100-form-btn">
-                    Login
-            </button>
-                </div>
-                <div className="text-center p-t-12">
-                  <span className="txt1">
-                    Forgot
-            </span>
-                  <a className="txt2" href="#">
-                    Username / Password?
-            </a>
-                </div>
-                <div className="text-center p-t-136">
-                  <a className="txt2" href="#">
-                    Create your Account
-              <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </form>
-          </div>
-        </div>
-      </div>
-    </>
+    <Container className="signup">
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <h1>Home Page</h1>
+          {isAuth ? (
+            <>
+              <Button
+                className="m-1"
+                onClick={e => {
+                  e.preventDefault();
+                  setSecret('');
+                  logout();
+                }}
+              >
+                Logout
+              </Button>
+              <Button
+                className="m-1"
+                onClick={e => {
+                  e.preventDefault();
+                  props.history.push("/members");
+                }}
+              >
+                Members
+              </Button>
+            </>
+          ) : (
+              <>
+                <Button
+                  className="m-1"
+                  onClick={e => {
+                    e.preventDefault();
+                    props.history.push("/login");
+                  }}
+                >
+                  Login
+              </Button>
+                <Button
+                  className="m-1"
+                  onClick={e => {
+                    e.preventDefault();
+                    props.history.push("/signup");
+                  }}
+                >
+                  Signup
+              </Button>
+              </>
+            )}
+          <Button
+            className="m-1"
+            onClick={e => {
+              e.preventDefault();
+              getSecret();
+            }}
+          >
+            Show Secrets
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <h1>{secret}</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
-export default home;
-         // import React from 'react';
+
+export default Home;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { form, span } from 'react-bootstrap/Form';
+// import './css/main.css'
+// import './css/util.css'
+// import pic from './images/img-01.png';
+// import './vendor/animate/animate.css';
+// import MyNavbar from "../../components/Navbar/Navbar.js";
+
+// function home() {
+//   return (
+//     <>
+//     <MyNavbar/>
+//       <div className="limiter">
+//         <div className="container-login100">
+//           <div className="wrap-login100">
+//               <img src={pic} alt="not working" />
+//               <form className="login100-form validate-form">
+//                 <span className="login100-form-title"> Member Login </span>
+//                 <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+//                   <input className="input100" type="text" name="email" placeholder="Email" />
+//                   <span className="focus-input100"></span>
+//                   <span className="symbol-input100">
+//                     <i className="fa fa-envelope" aria-hidden="true"></i>
+//                   </span>
+//                 </div>
+//                 <div className="wrap-input100 validate-input" data-validate="Password is required">
+//                   <input className="input100" type="password" name="pass" placeholder="Password" />
+//                   <span className="focus-input100"></span>
+//                   <span className="symbol-input100">
+//                     <i className="fa fa-lock" aria-hidden="true"></i>
+//                   </span>
+//                 </div>
+//                 <div className="container-login100-form-btn">
+//                   <button className="login100-form-btn">
+//                     Login
+//             </button>
+//                 </div>
+//                 <div className="text-center p-t-12">
+//                   <span className="txt1">
+//                     Forgot
+//             </span>
+//                   <a className="txt2" href="#">
+//                     Username / Password?
+//             </a>
+//                 </div>
+//                 <div className="text-center p-t-136">
+//                   <a className="txt2" href="#">
+//                     Create your Account
+//               <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+//                   </a>
+//                 </div>
+//               </form>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// export default home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
 // import {Jumbotron, Button} from 'reactstrap';
           // import MediaControlCard from '../../components/myHomeCard.js';
           // import CssBaseline from '@material-ui/core/CssBaseline';
