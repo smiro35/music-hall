b5961e2740fee110a3895609a7517de1f91390bb
Shakur <miiro.shakur@gmail.com>
2020-03-11 13:24:20 -0400

    dashboard design

--- client/src/components/Container/Container.js
+++ client/src/components/Container/Container.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import{Container,Row,Col} from "react-bootstrap"
 
 
-export default function SimpleContainer() {
+function container() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" style={{marginTop:"1rem",marginRight:"3rem", width:"100vw"}}>
-       
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-        
-
-        </Typography>
-        
-      </Container>
-    </React.Fragment>
+    <Container>
+  <Row>
+    <Col sm={4}>sm=4</Col>
+    <Col md={8}>md=8</Col>
+  </Row>
+  <Row>
+    <Col md>md=true</Col>
+    
+  </Row>
+</Container>
   );
 }
+
+
+export default container
 
 
