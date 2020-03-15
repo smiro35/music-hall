b5675cdf2aeb501cff0577520adfe9c0065a65f2
Shakur <miiro.shakur@gmail.com>
2020-03-15 02:44:56 -0400

    changed JawsDB

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
   },
   production: {
     use_env_variable: 'JAWSDB_URL',
-    username: `jto5u6ll0x3yp602`,
-    password: `jpg4v9uam81o2rod`,
-    database: `qw0tfgf5kf1riikq`,
-    host: `umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306`,
+    username: 'jto5u6ll0x3yp602',
+    password: 'jpg4v9uam81o2rod',
+    database: 'qw0tfgf5kf1riikq',
+    host: 'umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    Port:'3306',
     dialect: 'mysql',
   },
 
