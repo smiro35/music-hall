19d0ec38341b8302fdc0b7fdb270bb53e3710882
Shakur <miiro.shakur@gmail.com>
2020-03-15 14:36:08 -0400

    changing index routing

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api', routes);
 // Everything that is not an api request is sent to index.html
 // for client side routing.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
 });
 
 // Sync sequelize models then start Express app
